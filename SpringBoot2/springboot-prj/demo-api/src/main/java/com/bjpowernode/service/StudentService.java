610dbb86e0537f71a6f71c7caddc05421ce4c3e0
Sunwenzhi58 <sunwenzhi40@gmail.com>
2023-06-29 22:34:18 +0800

    springboot2 first commit

---
+++ SpringBoot2/springboot-prj/demo-api/src/main/java/com/bjpowernode/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.service;
+
+import com.bjpowernode.model.Student;
+
+//Dubbo接口
+public interface StudentService {
+
+    int addStudent(Student student);
+
+    Student queryStudent(Integer id);
+}
